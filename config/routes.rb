c08b1f60d2b860c99d2e887c3a6b6b5d7aa3960e
Derek Barber <derek@greylevel.com>
2014-06-02 08:55:03 -0700

    Update routes for agents, department & home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :agents
+  resources :departments
+
+  resources :agents
+
+  resources :organizations
+
+
+  root to: 'home#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
