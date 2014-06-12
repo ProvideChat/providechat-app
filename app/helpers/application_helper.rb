946b5fac9432439f7af7e5c5b547943d96ced7cb
Derek Barber <derek@greylevel.com>
2014-06-12 06:20:46 -0700

    Add flash messages partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
     action.include?(params[:action])
   end
 
+  def flash_class(level)
+    case level
+    when 'notice' then "info"
+    when 'success' then "success"
+    when 'error' then "danger"
+    when 'alert' then "warning"
+    end
+  end
+
 end
