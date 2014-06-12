487050c6f785813a5fdc8cb33697c2aa7cc2e175
Derek Barber <derek@greylevel.com>
2014-06-12 06:30:33 -0700

    Add processing for uploaded avatars

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -22,17 +22,19 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  process :resize_to_fit => [60, 60]
+
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  #process :scale => [200, 300]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  #version :thumb do
+  #  process :resize_to_fit => [60, 60]
+  #end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
