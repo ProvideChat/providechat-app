c4067e26466c3f776630193df300314d796f67fd
Derek Barber <derek@greylevel.com>
2014-06-19 06:32:03 -0700

    Various view updates

--- app/models/department.rb
+++ app/models/department.rb
@@ -4,4 +4,9 @@ class Department < ActiveRecord::Base
   
   enum status: [:disabled, :enabled]
   
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+  
 end
