a854a5b399521c7acde06170332d4e62c0238751
Derek Barber <derek@greylevel.com>
2014-06-20 20:35:48 -0700

    Change how organization billing is tracked on a per agent basis

--- db/migrate/20140528140214_create_organizations.rb
+++ db/migrate/20140528140214_create_organizations.rb
@@ -3,7 +3,7 @@ class CreateOrganizations < ActiveRecord::Migration
     create_table :organizations do |t|
       t.string :name
       t.string :email
-      t.integer :edition
+      t.integer :max_agents
       t.integer :payment_system
       t.integer :inactive_visitor_removal
       t.integer :agent_session_timeout
