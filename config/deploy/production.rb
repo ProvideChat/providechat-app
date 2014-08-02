c7ba343293d092d366d52e152bb8f5dccf933270
Derek Barber <derek@greylevel.com>
2014-08-02 08:04:50 -0700

    Update Capistrano configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
 server '107.170.65.78', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
-set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:full_app_name)}"
 
 set :rails_env, :production
 
