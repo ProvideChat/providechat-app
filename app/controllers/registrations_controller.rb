239b2a911b23eb8c2e3bc4b59e8bc10976674168
Derek Barber <derek@greylevel.com>
2014-09-11 18:32:39 -0700

    Get registration code to execute during create action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,25 @@
 class RegistrationsController < Devise::RegistrationsController
+  
+  def create
+    super
+
+    if resource.save
+      organization = Organization.new
+      organization.email = resource.email
+      organization.account_type = 'trial'
+      organization.status = 'enabled'
+      organization.payment_system = 'stripe'
+      organization.save
+  
+      resource.account_type = 'superadmin'
+      resource.organization_id = organization.id
+      resource.save
+    end
+    #sign_in(resource_name, resource)
+  end
+  
   protected
   
-  def after_sign_up_path_for(resource)
-
-    organization = Organization.new
-    organization.email = resource.email
-    organization.account_type = 'trial'
-    organization.status = 'enabled'
-    organization.payment_system = 'stripe'
-    organization.save
-  
-    resource.account_type = 'superadmin'
-    resource.organization_id = organization.id
-    resource.save
-  
-    sign_in(resource_name, resource)
-  end
+  #def after_sign_up_path_for(resource)
+  #end
 end
