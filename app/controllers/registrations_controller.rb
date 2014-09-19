b3d8f0a5c840bfe3675c71fb91892fb5c4d1eca9
Derek Barber <db@derekbarber.ca>
2014-09-19 15:07:56 -0700

    Set max agents upon initial registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-  
+
   def create
     super
 
@@ -7,19 +7,20 @@ class RegistrationsController < Devise::RegistrationsController
       organization = Organization.new
       organization.email = resource.email
       organization.account_type = 'trial'
+      organization.max_agents = 1
       organization.status = 'enabled'
       organization.payment_system = 'stripe'
       organization.save
-  
+
       resource.account_type = 'superadmin'
       resource.organization_id = organization.id
       resource.save
     end
     #sign_in(resource_name, resource)
   end
-  
+
   protected
-  
+
   #def after_sign_up_path_for(resource)
   #end
 end
