b3c87224243730f03b8b05c9ddc038b321584116
Derek Barber <db@derekbarber.ca>
2014-10-09 14:57:04 -0700

    Add support for geolocation fields when processing a visitor

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -2,7 +2,7 @@ class Visitor < ActiveRecord::Base
   has_one :chat
 
   enum status: [:no_chat, :waiting_to_chat, :in_chat, :chat_ended, :offsite]
-  
+
 #  t.integer  "organization_id"
 #  t.integer  "website_id"
 #  t.string   "name"
@@ -35,25 +35,37 @@ class Visitor < ActiveRecord::Base
     # {"api_version":0.4,
     #  "locale":{"country":"us","lang":"en"},
     # "current_session":
-    #  SESSION DETAILS: {"api_version"=>0.4, "locale"=>{"country"=>"us", "lang"=>"en"}, "current_session"=>{"visits"=>1, "start"=>1411186442747, "last_visit"=>1411186442747, "url"=>"http://new.providechat.dev/", "path"=>"/", "referrer"=>"", "referrer_info"=>{"host"=>"new.providechat.dev", "path"=>"/", "protocol"=>"http:", "port"=>80, "search"=>"", "query"=>{}}, "search"=>{"engine"=>nil, "query"=>nil}}, "original_session"=>{"visits"=>16, "start"=>1411184787036, "last_visit"=>1411186442748, "url"=>"http://new.providechat.dev/", "path"=>"/", "referrer"=>"", "referrer_info"=>{"host"=>"new.providechat.dev", "path"=>"/", "protocol"=>"http:", "port"=>80, "search"=>"", "query"=>{}}, "search"=>{"engine"=>nil, "query"=>nil}, "prev_visit"=>1411186343610, "time_since_last_visit"=>99138}, "browser"=>{"browser"=>"Chrome", "version"=>37, "os"=>"Mac"}, "plugins"=>{"flash"=>true, "silverlight"=>true, "java"=>false, "quicktime"=>true}, "time"=>{"tz_offset"=>-7, "observes_dst"=>true}, "device"=>{"screen"=>{"width"=>1366, "height"=>768}, "viewport"=>{"width"=>1175, "height"=>669}, "is_tablet"=>false, "is_phone"=>false, "is_mobile"=>false}, "location"=>{"error"=>true, "source"=>"google"}} 
+    #  SESSION DETAILS: {"api_version"=>0.4, "locale"=>{"country"=>"us", "lang"=>"en"}, "current_session"=>{"visits"=>1, "start"=>1411186442747, "last_visit"=>1411186442747, "url"=>"http://new.providechat.dev/", "path"=>"/", "referrer"=>"", "referrer_info"=>{"host"=>"new.providechat.dev", "path"=>"/", "protocol"=>"http:", "port"=>80, "search"=>"", "query"=>{}}, "search"=>{"engine"=>nil, "query"=>nil}}, "original_session"=>{"visits"=>16, "start"=>1411184787036, "last_visit"=>1411186442748, "url"=>"http://new.providechat.dev/", "path"=>"/", "referrer"=>"", "referrer_info"=>{"host"=>"new.providechat.dev", "path"=>"/", "protocol"=>"http:", "port"=>80, "search"=>"", "query"=>{}}, "search"=>{"engine"=>nil, "query"=>nil}, "prev_visit"=>1411186343610, "time_since_last_visit"=>99138}, "browser"=>{"browser"=>"Chrome", "version"=>37, "os"=>"Mac"}, "plugins"=>{"flash"=>true, "silverlight"=>true, "java"=>false, "quicktime"=>true}, "time"=>{"tz_offset"=>-7, "observes_dst"=>true}, "device"=>{"screen"=>{"width"=>1366, "height"=>768}, "viewport"=>{"width"=>1175, "height"=>669}, "is_tablet"=>false, "is_phone"=>false, "is_mobile"=>false}, "location"=>{"error"=>true, "source"=>"google"}}
 
     #Rails.logger.debug "current_session.visits: #{session['current_session']['visits']}"
 
     #remote_addr = session['']
     current_visits = session['current_session']['visits']
     total_visits = session['original_session']['visits']
-    remote_host = session['current_session']['referrer_info']['host']
+    #remote_host = session['current_session']['referrer_info']['host']
     current_page = session['current_session']['url']
-    country = session['locale']['country']
+    #country = session['locale']['country']
     language = session['locale']['lang']
     referrer_host = session['current_session']['referrer_info']['host']
     referrer_path = session['current_session']['referrer_info']['path']
     referrer_search = session['current_session']['referrer_info']['search']
     #referrer_query = session['current_session']['referrer_info']['query']
-    
+
+    ip_address = session['location']['ip']
+    latitude = session['location']['latitude']
+    longitude = session['location']['longitude']
+    country_code = session['location']['country_code']
+    country_name = session['location']['country_name']
+    city = session['location']['city']
+    region_code = session['location']['region_code']
+    region_name = session['location']['region_name']
+    zipcode = session['location']['zipcode']
+    area_code = session['location']['area_code']
+    metro_code = session['location']['metro_code']
+
     search_engine = session['current_session']['search']['engine']
     search_query = session['current_session']['search']['query']
-    
+
     browser_name = session['browser']['browser']
     browser_version = session['browser']['version']
     operating_system = session['browser']['os']
@@ -63,13 +75,13 @@ class Visitor < ActiveRecord::Base
     website = Website.find_by(:organization_id => org_id, :url => referrer_host)
 
     if website
-      visitor = Visitor.find_by(:website_id => website.id) || Visitor.new
+      visitor = Visitor.find_by(:website_id => website.id, :browser_name => browser_name) || Visitor.new
 
       visitor.organization_id = org_id
       visitor.website_id = website.id
-      visitor.country = country
+      #visitor.country = country
       visitor.current_page = current_page
-      visitor.remote_host = remote_host
+      #visitor.remote_host = remote_host
       visitor.page_views = current_visits
 
       visitor.language = language
@@ -77,7 +89,7 @@ class Visitor < ActiveRecord::Base
       visitor.referrer_path = referrer_path
       visitor.referrer_search = referrer_search
       #visitor.referrer_query = referrer_query
-      
+
       visitor.search_engine = search_engine
       visitor.search_query = search_query
 
@@ -85,7 +97,19 @@ class Visitor < ActiveRecord::Base
       visitor.browser_version = browser_version
       visitor.operating_system = operating_system
       visitor.screen_resolution = screen_resolution
-   
+
+      visitor.ip_address = ip_address
+      visitor.latitude = latitude
+      visitor.longitude = longitude
+      visitor.country_code = country_code
+      visitor.country_name = country_name
+      visitor.city = city
+      visitor.region_code = region_code
+      visitor.region_name = region_name
+      visitor.zipcode = zipcode
+      visitor.area_code = area_code
+      visitor.metro_code = metro_code
+
       if session['location']['error']
         Rails.logger.debug "No location data"
       end
