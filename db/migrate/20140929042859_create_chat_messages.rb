cabe23cf16cb405f3e15d9bd3a0ffb5177ba6d2c
Derek Barber <db@derekbarber.ca>
2014-10-08 21:55:46 -0700

    Update ChatMessages with support for enum fields

--- db/migrate/20140929042859_create_chat_messages.rb
+++ db/migrate/20140929042859_create_chat_messages.rb
@@ -3,11 +3,11 @@ class CreateChatMessages < ActiveRecord::Migration
     create_table :chat_messages do |t|
       t.integer :chat_id
       t.string :user_name
-      t.string :sender
-      t.string :message_type
+      t.integer :sender
+      t.integer :message_type
       t.timestamp :sent
-      t.boolean :seen_by_agent
-      t.boolean :seen_by_visitor
+      t.boolean :seen_by_agent, :default => false
+      t.boolean :seen_by_visitor, :default => false
       t.text :message
 
       t.timestamps
