a4dbb570b0c3e6bd337afae093db24a236586119
Derek Barber <derek@greylevel.com>
2014-10-10 22:05:13 -0700

    Simplify invitation fields and use enums

--- db/migrate/20140724171120_create_invitations.rb
+++ db/migrate/20140724171120_create_invitations.rb
@@ -4,8 +4,7 @@ class CreateInvitations < ActiveRecord::Migration
       t.integer :organization_id
       t.integer :website_id
       t.string :invitation_message
-      t.boolean :smart_invite_enabled
-      t.string :smart_invite_mode
+      t.integer :invite_mode
       t.integer :invite_pageviews
       t.integer :invite_seconds
 
