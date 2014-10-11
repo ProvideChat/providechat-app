00298d0f65190f9504747d91b2388beb0db0e435
Derek Barber <derek@greylevel.com>
2014-10-10 22:27:02 -0700

    Fix issue with enum

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,12 +3,12 @@ class Invitation < ActiveRecord::Base
   belongs_to :website
   before_create :set_defaults
   
-  enum :invite_mode [:none , :page_views, :seconds]
+  enum invite_mode: [:disabled, :page_views, :seconds]
   
   private
     def set_defaults
       self.invitation_message = "Hi there, I'm %name%, is there anything I can help you with today?"
-      self.invite_mode = "none"
+      self.invite_mode = "disabled"
       self.invite_pageviews = 3
       self.invite_seconds = 15
     end
