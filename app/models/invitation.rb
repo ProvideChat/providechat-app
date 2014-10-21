7339061d711947ba0ca7091a8145fe312741d509
Derek Barber <db@derekbarber.ca>
2014-10-21 16:59:37 -0700

    Implement basic invitation functionality

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,9 +2,9 @@ class Invitation < ActiveRecord::Base
   belongs_to :organization
   belongs_to :website
   before_create :set_defaults
-  
-  enum invite_mode: [:disabled, :page_views, :seconds]
-  
+
+  enum invite_mode: [:no_invite, :page_views, :seconds]
+
   private
     def set_defaults
       self.invitation_message = "Hi there, I'm %name%, is there anything I can help you with today?"
