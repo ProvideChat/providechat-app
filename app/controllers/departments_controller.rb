11e70aab72d97a0d99a260d6d9c1dc9d2124a00d
Derek Barber <db@derekbarber.ca>
2014-10-21 15:48:07 -0700

    Improve departments controller and remove json responses

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,6 +1,7 @@
 class DepartmentsController < ApplicationController
   before_action :authenticate_agent!
   before_action :set_department, only: [:edit, :update, :destroy]
+  before_action :set_websites, only: [:edit, :new]
 
   def index
     @departments = Department.where(organization_id: current_agent.organization_id)
@@ -18,40 +19,33 @@ class DepartmentsController < ApplicationController
     @department.organization_id = current_agent.organization_id
     @department.status = 'enabled'
 
-    respond_to do |format|
-      if @department.save
-        format.html { redirect_to departments_url, notice: 'Department was successfully created.' }
-        format.json { render :show, status: :created, location: departments_url }
-      else
-        format.html { render :new }
-        format.json { render json: @department.errors, status: :unprocessable_entity }
-      end
+    if @department.save
+      redirect_to departments_url, notice: 'Department was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @department.update(department_params)
-        format.html { redirect_to departments_url, notice: 'Department was successfully updated.' }
-        format.json { render :show, status: :ok, location: departments_url }
-      else
-        format.html { render :edit }
-        format.json { render json: @department.errors, status: :unprocessable_entity }
-      end
+    if @department.update(department_params)
+      redirect_to departments_url, notice: 'Department was successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
     @department.destroy
-    respond_to do |format|
-      format.html { redirect_to departments_url, notice: 'Department was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to departments_url, notice: 'Department was successfully destroyed.'
   end
 
   private
     def set_department
       @department = Department.find(params[:id])
+    end
+
+    def set_websites
+      @websites = Website.where(organization_id: current_agent.organization_id)
     end
 
     def department_params
--- app/views/departments/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@departments) do |department|
-  json.extract! department, :id, :organization_id, :name, :email, :status
-  json.url department_url(department, format: :json)
-end
