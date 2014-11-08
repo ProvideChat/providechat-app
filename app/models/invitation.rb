34df2e7c46c3e663e0398adf5281181aa940e5fe
Derek Barber <derek@greylevel.com>
2014-11-07 21:52:16 -0800

    Set a default invitation to page_views

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -6,10 +6,11 @@ class Invitation < ActiveRecord::Base
   enum invite_mode: [:no_invite, :page_views, :seconds]
 
   private
-    def set_defaults
-      self.invitation_message = "Hi there, is there anything I can help you with today?"
-      self.invite_mode = "no_invite"
-      self.invite_pageviews = 3
-      self.invite_seconds = 15
-    end
+  
+  def set_defaults
+    self.invitation_message = "Hi there, is there anything I can help you with today?"
+    self.invite_mode = "page_views"
+    self.invite_pageviews = 3
+    self.invite_seconds = 15
+  end
 end
