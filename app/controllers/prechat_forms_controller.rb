06f76e7a88e0988620d65b649d45d6e4c06e9d2b
Derek Barber <derek@greylevel.com>
2014-11-28 23:14:08 -0800

    Implement prechat forms cancel & restore default

--- app/controllers/prechat_forms_controller.rb
+++ app/controllers/prechat_forms_controller.rb
@@ -21,7 +21,8 @@ class PrechatFormsController < ApplicationController
   def update
     @prechat_form = PrechatForm.find(params[:id])
 
-    if @prechat_form.update(prechat_form_params)
+    #if @prechat_form.update(prechat_form_params)
+    if flash_message = @prechat_form.process_update(params, prechat_form_params)      
       redirect_to edit_prechat_form_path(@prechat_form), notice: 'Prechat survey was successfully updated.'
     else
       render :edit
--- app/models/prechat_form.rb
+++ app/models/prechat_form.rb
@@ -3,15 +3,30 @@ class PrechatForm < ActiveRecord::Base
   belongs_to :website
   before_create :set_defaults
   
-  private
-    def set_defaults
-      self.intro_text = 'Thank you for contacting us!'
-      self.name_text = 'Name'
-      self.email_text =  'Email'
-      self.email_enabled = true
-      self.department_text = 'Department'
-      self.department_enabled = true
-      self.message_text = 'Initial Question'
-      self.button_text = 'Start Chat'
+  def process_update(params, prechat_form_params)
+    if params.has_key?(:restore_defaults)
+      self.set_defaults
+      self.save
+      "Your prechat form was reset to defaults."
+    elsif params.has_key?(:save_changes)
+      self.update(prechat_form_params)
+      self.save
+      "Your prechat form was successfully updated."
+    else
+      "Your changes were cancelled"
     end
+  end
+  
+  protected
+  
+  def set_defaults
+    self.intro_text = 'Thank you for contacting us!'
+    self.name_text = 'Name'
+    self.email_text =  'Email'
+    self.email_enabled = true
+    self.department_text = 'Department'
+    self.department_enabled = true
+    self.message_text = 'Initial Question'
+    self.button_text = 'Start Chat'
+  end
 end
