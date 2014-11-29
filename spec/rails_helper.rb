3a9cf47d41fbec4085a06e4adaa9a98690211f37
Derek Barber <db@derekbarber.ca>
2014-11-28 17:27:17 -0800

    Add capybara with url blacklisting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,4 +40,10 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  Capybara.javascript_driver = :webkit
+  config.before(:each, js: true) do
+    page.driver.browser.url_blacklist = ["http://fonts.googleapis.com"]
+  end
+
 end
