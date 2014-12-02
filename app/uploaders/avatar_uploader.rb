05c3befaa51f7d87b11c4a053b45897b144989a2
Derek Barber <derek@greylevel.com>
2014-12-01 16:40:16 -0800

    Implement default image for agent photo

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,9 +9,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MimeTypes
   process :set_content_type
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  #def store_dir
+  #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -21,7 +21,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
   def default_url
-    ActionController::Base.helpers.image_path("silhouette.png")
+    '/images/silhouette.png'
   end
 
   process :resize_to_fit => [60, 60]
