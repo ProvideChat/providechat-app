efcb038a4268801fa43fac1b88566db1f5572dda
Derek Barber <db@derekbarber.ca>
2014-12-04 17:37:32 -0800

    Create a new qtip helper function

--- app/assets/javascripts/qtip2_custom.js
+++ app/assets/javascripts/qtip2_custom.js
@@ -18,6 +18,23 @@ function default_style(content, field) {
   return style;
 };
 
+function on_focus_style(content, field) {
+  var style = {
+    content: content,
+    hide: {
+      event: 'unfocus'
+    },
+    position: {
+      my: 'bottom right',  // Position my top left...
+      at: 'top right', // at the bottom right of...
+      adjust: { x: 20 },
+      target: $(field) // my target
+    }
+  };
+
+  return style;
+}
+
 function no_icon_style(content, field) {
   var style = {
     content: content,
