0481fe5d49f4f9bd38a7bd4e12a699c4fd7ed0d1
Derek Barber <db@derekbarber.ca>
2014-12-05 17:02:17 -0800

    Add Devise helper for devise_error_messages method

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,17 @@
+module DeviseHelper
+ def devise_error_messages!
+  return '' if resource.errors.empty?
+
+   messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+   html = <<-HTML
+   <div class="alert alert-danger alert-dismissable fade in"> <button type="button"
+    class="close" data-dismiss="alert">x</button>
+    <strong>Please fix the following #{"error".pluralize(resource.errors.count)}:</strong>
+    #{messages}
+   </div>
+   HTML
+
+   html.html_safe
+ end
+end
