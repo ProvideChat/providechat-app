488244726050e9e7be3e549a4d4f594755747bf2
Derek Barber <db@derekbarber.ca>
2014-12-07 15:32:26 -0800

    Implement Reset & Cancel button functionality for Chat Widgets

--- app/controllers/chat_widgets_controller.rb
+++ app/controllers/chat_widgets_controller.rb
@@ -20,24 +20,20 @@ class ChatWidgetsController < ApplicationController
   def update
     @chat_widget = ChatWidget.find(params[:id])
 
-    respond_to do |format|
-      if @chat_widget.update(chat_widget_params)
-        format.html { redirect_to edit_chat_widget_path(@chat_widget), notice: 'Chat widget was successfully updated.' }
-        format.json { render :show, status: :ok, location: @chat_widget }
-      else
-        format.html { render :edit }
-        format.json { render json: @chat_widget.errors, status: :unprocessable_entity }
-      end
+    if flash_message = @chat_widget.process_update(params, chat_widget_params)
+      redirect_to edit_chat_widget_path(@chat_widget), notice: 'Chat widget was successfully updated.'
+    else
+      render :edit
     end
   end
 
   private
-    def set_websites
-      @websites = Website.where(organization_id: current_agent.organization_id)
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def chat_widget_params
-      params.require(:chat_widget).permit(:online_message, :offline_message, :title_message, :color, :display_logo, :display_agent_avatar, :display_mobile_icon)
-    end
+  def set_websites
+    @websites = Website.where(organization_id: current_agent.organization_id)
+  end
+
+  def chat_widget_params
+    params.require(:chat_widget).permit(:online_message, :offline_message, :title_message, :hide_when_offline, :color, :display_logo, :display_agent_avatar, :display_mobile_icon)
+  end
 end
