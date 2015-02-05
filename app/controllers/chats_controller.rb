0309b0ac24985c762d8626da31d459453700c4a5
Derek Barber <db@derekbarber.ca>
2015-02-05 13:32:13 -0800

    Implement chat transcript search with ajax

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,13 +1,24 @@
 class ChatsController < ApplicationController
 
   def index
-    @chats = Chat.where(organization_id: current_agent.organization_id)
     @agents = Agent.where(organization_id: current_agent.organization_id)
     @websites = Website.where(organization_id: current_agent.organization_id)
+
+    @chats = Chat.filter_results(current_agent.organization_id, filtering_params(params))
+
+    respond_to do |format|
+      format.html
+      format.js
+     end
   end
 
   def show
     @chat = Chat.find(params[:id])
+  end
+
+private
+  def filtering_params(params)
+    params.slice(:chat_id, :visitor_email, :website_ids, :agent_ids, :from_date, :to_date)
   end
 
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -7,9 +7,27 @@ class Chat < ActiveRecord::Base
 
   has_many :chat_messages
 
+  scope :websites, -> (website_ids) { where website_id: website_ids }
+  scope :department, -> (department) { where visitor_department: department }
+  scope :agents, -> (agent_ids) { where agent_id: agent_ids }
+  scope :from_date, -> (from_date) { where("created_at >= ?", from_date.to_date) }
+  scope :to_date, -> (to_date) { where("created_at <= ?", to_date.to_date) }
+
   before_save :titleize_visitor_name
 
   enum status: [:not_started , :in_progress, :agent_ended, :visitor_ended, :agent_timeout, :visitor_timeout]
+
+  def self.filter_results(organization_id, params)
+    chats = Chat.where(organization_id: organization_id)
+    chats = chats.websites(params[:website_ids]) if params[:website_ids].present?
+    chats = chats.agents(params[:agent_ids]) if params[:agent_ids].present?
+    chats = chats.from_date(params[:from_date]) if params[:from_date].present?
+    chats = chats.to_date(params[:to_date]) if params[:to_date].present?
+
+    chats = chats.find(params[:chat_id]) if params[:chat_id].present?
+
+    chats
+  end
 
   def end_chat(reason)
     self.status = reason
