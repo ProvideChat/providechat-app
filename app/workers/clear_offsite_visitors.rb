1aa12c345d71157972693b4178d45710187c02ad
Derek Barber <db@derekbarber.ca>
2015-03-13 15:02:08 -0700

    Update ClearOffsetVisitors background job

--- app/workers/clear_offsite_visitors.rb
+++ app/workers/clear_offsite_visitors.rb
@@ -2,10 +2,10 @@ class ClearOffsiteVisitors
   include Sidekiq::Worker
 
   def perform
-    visitors = Visitor.where("status <> ? AND last_ping < ?", Visitor.statuses[:in_chat], 5.minutes.ago)
+    visitors = Visitor.where("status = ? AND last_ping < ?", Visitor.statuses[:in_chat], 5.minutes.ago)
 
     visitors.each do |visitor|
-      visitor.status = 'offsite'
+      visitor.status = Visitor.statuses[:offsite]
       visitor.save
     end
   end
