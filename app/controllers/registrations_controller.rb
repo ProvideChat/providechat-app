7b79bab3daecb13a3bf702a21d639660ebf742bd
Derek Barber <derek@greylevel.com>
2015-05-01 21:48:44 -0700

    Get basic registration working

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,13 +2,22 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super
 
-    return unless resource.save
     organization = Organization.create_default_organization
 
+    resource.name = 'Agent'
+    resource.title = 'Support Hero'
     resource.access_level = 'superadmin'
     resource.organization_id = organization.id
     resource.save
+  end
 
-    # sign_in(resource_name, resource)
+  private
+  
+  def sign_up_params
+    params.require(:agent).permit(:email, :password)
+  end
+
+  def account_update_params
+    params.require(:agent).permit(:name, :email, :password, :password_confirmation, :current_password)
   end
 end
