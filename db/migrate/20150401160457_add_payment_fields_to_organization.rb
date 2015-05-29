7a918333f80ea1a7b471f3dd9c56a0e235158bff
Derek Barber <derek@greylevel.com>
2015-05-29 16:02:13 -0700

    Remove null requirement for trial_period_end

--- db/migrate/20150401160457_add_payment_fields_to_organization.rb
+++ db/migrate/20150401160457_add_payment_fields_to_organization.rb
@@ -1,8 +1,8 @@
 class AddPaymentFieldsToOrganization < ActiveRecord::Migration
   def change
     add_column :organizations, :trial_days, :integer, :default => 14, :null => false
-    add_column :organizations, :trial_period_end, :timestamp, :null => false
-    add_column :organizations, :stripe_customer_id, :string, :default => ''
+    add_column :organizations, :trial_period_end, :timestamp
+    add_column :organizations, :stripe_customer_id, :string
     remove_column :organizations, :max_agents
   end
 end
