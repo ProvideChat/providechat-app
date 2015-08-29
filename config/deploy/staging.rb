b25e689bfd70b844d3c5a1ed1c4bb1d63d9da174
Derek Barber <derek@greylevel.com>
2015-08-28 23:02:07 -0700

    Update Capistrano for puma

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,4 +8,10 @@ server '104.131.79.154', user: 'deploy', roles: %w{web app}, my_property: :my_va
 
 set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:full_app_name)}"
 
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:full_app_name)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+
 set :rails_env, :staging
