16ad0c7d3cecbbbb68f8cc42b2db6fdb12777ad9
Derek Barber <derek@greylevel.com>
2015-10-30 23:00:34 -0700

    Updates to chat transcripts
    This update fixes some bugs with chat transcripts and also load the
    transcript in a modal window to improve the navigation.

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -16,6 +16,11 @@ class ChatsController < ApplicationController
   
   def show
     @chat = Chat.find(params[:id])
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
