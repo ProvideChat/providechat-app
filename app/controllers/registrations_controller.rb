cf87434f6ed45f9e7d96e6c56499d3fe107cdfa7
Derek Barber <db@derekbarber.ca>
2015-12-13 21:57:01 -0800

    Don't verify authenticity token for sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
+  skip_before_filter :verify_authenticity_token, :only => :create
+
   def create
     super
-    
+
     organization = Organization.create_default_organization
 
     resource.title = 'Support Hero'
     resource.access_level = 'superadmin'
     resource.organization_id = organization.id
     resource.save(validate: false)
-    
+
     AgentMailer.welcome(resource.id).deliver_later
   end
 
   private
-  
+
   def sign_up_params
     params.require(:agent).permit(:email)
   end
