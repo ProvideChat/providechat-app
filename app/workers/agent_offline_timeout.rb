494970873771d7a69f48137d25b19d1721740b25
Derek Barber <db@derekbarber.ca>
2015-12-23 17:24:16 -0800

    Fix error in agent offline timeout worker

--- app/workers/agent_offline_timeout.rb
+++ app/workers/agent_offline_timeout.rb
@@ -2,6 +2,6 @@ class AgentOfflineTimeout
   include Sidekiq::Worker
 
   def perform
-   Agent.where("availability = ? AND last_seen_at < ?", Agent.availability[:online], 2.minutes.ago).update_all(availability: 'offline')
+    Agent.where("availability = ? AND last_seen_at < ?", Agent.availabilities[:online], 3.minutes.ago).update_all(availability: Agent.availabilities[:offline])
   end
 end
