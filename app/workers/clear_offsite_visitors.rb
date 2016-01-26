c12f7617210b09ad08f9b453c0effd9332cada70
Derek Barber <db@derekbarber.ca>
2016-01-26 10:31:58 -0800

    Improve offset visitor clearing job

--- app/workers/clear_offsite_visitors.rb
+++ app/workers/clear_offsite_visitors.rb
@@ -2,14 +2,25 @@ class ClearOffsiteVisitors
   include Sidekiq::Worker
 
   def perform
-    visitors = Visitor.where("status = ? AND last_ping < ?", Visitor.statuses[:in_chat], 5.minutes.ago)
+    chatting_visitors = Visitor.where("status = ? AND last_ping < ?", Visitor.statuses[:in_chat], 3.minutes.ago)
 
-    visitors.each do |visitor|
+    chatting_visitors.each do |visitor|
+      visitor.chat.status = Chat.statuses[:visitor_timeout]
+      visitor.chat.chat_ended = DateTime.now
+      visitor.chat.save
+
       visitor.status = Visitor.statuses[:offsite]
+      visitor.chat_id = 0
       visitor.save
+    end
 
-      chat = Chat.find(visitor.chat_id)
-      chat.end_chat('visitor_timeout')
+    not_chat_statuses = [ Visitor.statuses[:no_chat], Visitor.statuses[:waiting_to_chat],
+                        Visitor.statuses[:agent_ended], Visitor.statuses[:visitor_ended] ]
+    offsite_visitors = Visitor.where("status in (?) AND last_ping < ?", not_chat_statuses, 1.minute.ago)
+    offsite_visitors.each do |visitor|
+      visitor.status = Visitor.statuses[:offsite]
+      visitor.chat_id = 0
+      visitor.save
     end
   end
 end
