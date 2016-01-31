9468ffae18247320a19329494e65ec563fb874cd
Derek Barber <db@derekbarber.ca>
2016-01-31 15:03:12 -0800

    Add defaults to some fields in organizations table

--- db/migrate/20140528140214_create_organizations.rb
+++ db/migrate/20140528140214_create_organizations.rb
@@ -2,15 +2,15 @@ class CreateOrganizations < ActiveRecord::Migration
   def change
     create_table :organizations do |t|
 
-      t.integer :account_type, :default => 'trial', :null => false
-      t.integer :max_agents, :default => 1
-      t.integer :payment_system, :default => 'stripe'
-      t.integer :agent_session_timeout, :default => 30
-      t.integer :agent_response_timeout, :default => 2
+      t.integer :account_type, default: 'trial', null: false
+      t.integer :max_agents, default: 1
+      t.integer :payment_system, default: 'stripe'
+      t.integer :agent_session_timeout, default: 30
+      t.integer :agent_response_timeout, default: 2
 
-      t.boolean :completed_setup
+      t.boolean :completed_setup, default: false
 
-      t.integer :status
+      t.integer :status, default: 0
 
       t.timestamps null: false
     end
