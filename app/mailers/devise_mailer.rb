843a858be63ef616cdfdb57097c6b704536b6ca0
Derek Barber <db@derekbarber.ca>
2016-05-10 22:11:00 -0700

    Send devise emails from luke@providechat.com

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -3,7 +3,7 @@ class DeviseMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
 
-  default from: "noreply@providechat.com"
+  default from: "luke@providechat.com"
   layout 'mailer'
 
   add_template_helper(EmailHelper)
