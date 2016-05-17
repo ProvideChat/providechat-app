b53906cadeca4d159afc9d5d7f99dfdc72eec341
Derek Barber <db@derekbarber.ca>
2016-05-17 10:19:19 -0700

    Display time of chat message in transcript

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -2,4 +2,8 @@ class ChatMessage < ActiveRecord::Base
   belongs_to :chat
 
   enum sender: [:visitor, :agent, :system]
+
+  def sent_status(agent)
+    sent.in_time_zone(agent.time_zone).strftime('%l:%M %p')
+  end
 end
