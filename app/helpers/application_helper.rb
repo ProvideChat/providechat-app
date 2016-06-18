4a8015654c3c751fce9a5a1ab68a1f8e4087162c
Derek Barber <db@derekbarber.ca>
2016-06-17 22:58:52 -0700

    Add formatted_price helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,8 @@ module ApplicationHelper
   def flash_class(flash_type)
     BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym, '')
   end
+
+  def formatted_price(amount)
+    sprintf("$%0.2f", (amount || 0) / 100.0)
+  end
 end
