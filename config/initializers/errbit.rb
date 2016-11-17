93ab2d0719bee5c91d7cf90981b0ea4f4909d650
Derek Barber <db@derekbarber.ca>
2016-11-17 10:14:14 -0800

    Fix incorrect errbit project key

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
   config.host = 'https://providechat-errbit.herokuapp.com'
   config.project_id = 1 # required, but any positive integer works
-  config.project_key = ENV['errbit_project_key']
+  config.project_key = ENV['ERRBIT_PROJECT_KEY']
 
   # Uncomment for Rails apps
   config.environment = Rails.env
