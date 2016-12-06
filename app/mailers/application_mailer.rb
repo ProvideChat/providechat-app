c9af9c2c13bfac1d12c4699596934ac143bacef0
Derek Barber <db@derekbarber.ca>
2016-12-05 21:58:43 -0800

    Move all mailer views into their own mailer folder

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "noreply@providechat.com"
   layout 'mailer'
+  append_view_path Rails.root.join('app', 'views', 'mailers')
 
   add_template_helper EmailHelper
 end
